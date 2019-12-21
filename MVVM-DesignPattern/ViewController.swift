5f5e260494494bf670184aacd940f08380950410
Aman Aggarwal <aman@clickapps.co>
2019-12-21 14:18:04 +0530

    Initial Commit

---
+++ MVVM-DesignPattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM-DesignPattern
+//
+//  Created by Aman Aggarwal on 21/12/19.
+//  Copyright © 2019 Aman Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
